8ef5093a3aa43face8f986793271415343fb4f37
jokerav <jokeravdeveloper@gmail.com>
2022-08-01 19:07:24 +0300

    получил доступ к contcts.json

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Hello, world!");
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.normalize("db/contacts.json");
+fs.readFile(contactsPath)
+  .then((data) => console.log(data.toString()))
+  .catch((err) => console.log(err.message));
