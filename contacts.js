773cf5dd7a2cba2b53e4fc6ffe0266826333afe0
jokerav <jokeravdeveloper@gmail.com>
2022-08-03 23:22:01 +0300

    step 3

--- contacts.js
+++ contacts.js
@@ -1,11 +1,87 @@
-const fs = require("fs").promises;
+const fs = require("fs/promises");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const contactsPath = path.normalize("./db/contacts.json");
-function listContacts() {
-  fs.readFile(contactsPath)
-    .then((data) => console.log(data.toString()))
-    .catch((err) => console.log(err.message));
+const isContactInList = async (id) => {
+  const data = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+  let result = false;
+  const normID = id.toString();
+  data.map((contact) => {
+    if (contact.id.toString() === normID) {
+      result = true;
+    }
+  });
+  return result;
+};
+const isNameInList = async (name) => {
+  const data = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+  let result = false;
+  const normName = name.toLowerCase();
+  data.map((contact) => {
+    if (contact.name.toLowerCase() === normName) {
+      result = true;
+    }
+  });
+  return result;
+};
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    return data;
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-module.exports = { listContacts };
+async function getContactById(contactId) {
+  try {
+    const data = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+    return data.filter((contact) => contact.id === contactId.toString())[0];
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const inList = await isContactInList(contactId);
+    if (inList) {
+      const data = JSON.parse(await fs.readFile(contactsPath, "utf-8"));
+
+      const contacts = data.filter(
+        (contact) => contact.id !== contactId.toString()
+      );
+      console.log(contacts);
+      await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+async function addContact(name, email, phone) {
+  try {
+    let data = JSON.parse(await fs.readFile(contactsPath, "utf8"));
+    const contact = { name, email, phone, id: uuidv4() };
+    const iDIsLock = await isContactInList(contact.id);
+    const nameIsLock = await isNameInList(name);
+    console.log(iDIsLock);
+    console.log(nameIsLock);
+
+    if (!nameIsLock && !nameIsLock) {
+      data.push(contact);
+      await fs.writeFile(contactsPath, JSON.stringify(data));
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  isContactInList,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const contacts = require("./contacts.js");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  isContactInList,
+  addContact,
+} = require("./contacts.js");
 
 console.log("index.js");
-contacts.listContacts();
+// listContacts().then((data) => console.log(data));
+
+// getContactById(8).then((data) => console.log(data));
+// removeContact(10);
+// isContactInList(9).then((data) => console.log(data));
+addContact("John Black", "stupidJohnSnow@wall.com", "(323) 123-45-20").then(
+  (data) => console.log(data)
+);
